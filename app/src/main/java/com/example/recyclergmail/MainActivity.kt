8f322a0f85b632da7e1a30adacc15ce9afae7394
Davi Ximenes <davix.ufc@gmail.com>
2020-04-02 00:25:50 -0300

    add emails pressing the plus button working

--- app/src/main/java/com/example/recyclergmail/MainActivity.kt
+++ app/src/main/java/com/example/recyclergmail/MainActivity.kt
@@ -3,16 +3,48 @@ package com.example.recyclergmail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclergmail.model.email
 import com.example.recyclergmail.model.fakeEmails
+import com.mooveit.library.Fakeit
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var adapter: EmailAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fakeit.init()
         setContentView(R.layout.activity_main)
 
-        recycler_view_main.adapter = EmailAdapter(fakeEmails())
+        adapter = EmailAdapter(fakeEmails())
+        recycler_view_main.adapter = adapter
         recycler_view_main.layoutManager = LinearLayoutManager(this)
+
+        floatingActionButton.setOnClickListener {
+            addEmail()
+            recycler_view_main.scrollToPosition(0)
+        }
+    }
+    private fun addEmail(){
+        val fakeDate = SimpleDateFormat("MM/DD/YYYY", Locale("en","us")).parse(
+            Fakeit.dateTime().dateFormatter()
+        )
+        adapter.email.add(0,
+            email {
+                star = false
+                unread = true
+                userName = Fakeit.name().firstName()
+                emailSubject = Fakeit.company().name()
+                date = SimpleDateFormat("d MM",Locale("en","us")).format(fakeDate!!)
+                preview = mutableListOf<String>().apply {
+                    repeat(10){
+                        add(Fakeit.lorem().words())
+                    }
+                }.joinToString(" ")
+            })
+        adapter.notifyItemInserted(0)
     }
 }
